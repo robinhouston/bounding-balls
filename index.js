09540b11f2b8208592cb9a39300ad81a6ee290be
Robin Houston <robin.houston@gmail.com>
2017-05-14 18:54:38 +0100

    Minor tweaks to examples

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const B1 = {name: "B1", x: 400, y: 200, r: 100},
       B4 = {name: "B4", x: 490, y: 140, r: 3},
       B5 = {name: "B5", x: 490, y: 260, r: 3};
 
-const d = {name: "d", x: 400, y: 100, r: 20},
-      a = {name: "a", x: 370, y: 90, r: 5},
+const a = {name: "a", x: 365, y: 90, r: 5},
       b = {name: "b", x: 430, y: 90, r: 5},
       c = {name: "c", x: 400, y: 150, r: 10},
-      D = {name: "D", x: 450, y: 500, r: 10};
+      d = {name: "d", x: 400, y: 100, r: 30},
+      D = {name: "D", x: 400, y: 500, r: 10};
 
-const input_circles = [B1,B2,B3,B4,B5]; //[ d, D, c, b, a ];
+// const input_circles = [B1,B2,B3,B4,B5];
+const input_circles = [ d, D, c, b, a ];
 
 function allPermutationsOf(array, callback, level=0) {
     array = array.slice(0);
